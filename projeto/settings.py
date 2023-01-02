a62fdee64beca13a3662c982e6dc30655b5831a4
donovan-gomes <dnvn.dev@gmail.com>
2023-01-02 20:50:13 -0300

    Removido Contato e Sobre

--- projeto/settings.py
+++ projeto/settings.py
@@ -58,7 +58,6 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             BASE_DIR / 'base_templates',
-            BASE_DIR / 'recipes' / 'tempaltes_temp',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
